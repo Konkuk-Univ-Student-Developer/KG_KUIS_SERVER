114f1d9c1b65e0fa151fcfa0534676dddd632cc4
Minu Kim <kmw106933@naver.com>
2025-07-25 14:55:18 +0900

    [chore] 프로젝트 패키지 구성

---
+++ src/main/java/backend/kg_kuis_server/KgKuisServerApplication.java
@@ -0,0 +1,13 @@
+package backend.kg_kuis_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KgKuisServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KgKuisServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/backend/kg_kuis_server/KgKuisServerApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.kg_kuis_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KgKuisServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
